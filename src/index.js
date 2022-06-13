41b165f8fd27f52ff445941ef9f370b0e0b093e1
Agnieszka Kaminska <zabula81@o2.pl>
2022-06-13 10:40:54 +0200

    implemented hash router

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import "./index.css";
 import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
