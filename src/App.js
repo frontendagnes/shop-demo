16bb8c06be2186dca0afc8c2e7a8a29d787f8f08
Agnieszka Kaminska <zabula81@o2.pl>
2022-06-13 10:38:49 +0200

    added function suspense

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, Suspense } from "react";
 import "./App.css";
 
 import { Routes, Route } from "react-router-dom";
@@ -26,12 +26,15 @@ import OrderDetails from "./components/OrderDetails/OrderDetails";
 import NoMatch from "./components/NoMatch/NoMatch";
 import Footer from "./components/Footer/Footer";
 
+//mui
+import { CircularProgress } from "@mui/material";
+
 function App() {
   const [orders, setOrders] = useState([]);
 
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
-  
+
   useEffect(() => {
     onAuthStateChanged(auth, (authUser) => {
       if (authUser) {
@@ -44,91 +47,113 @@ function App() {
 
   useEffect(() => {
     if (user) {
-      const docRef = doc(db, "users", user.uid)
-      const ref  = collection(docRef, "orders")
-      const sortRef = query(ref, orderBy("created", "desc"))
+      const docRef = doc(db, "users", user.uid);
+      const ref = collection(docRef, "orders");
+      const sortRef = query(ref, orderBy("created", "desc"));
 
       const unsuscribe = onSnapshot(sortRef, (snapshot) => {
         setOrders(
-          snapshot.docs.map((doc) =>({
+          snapshot.docs.map((doc) => ({
             id: doc.id,
-            data: doc.data()
+            data: doc.data(),
           }))
-        )
-      })
-      return() =>{
-        unsuscribe()
-      }
+        );
+      });
+      return () => {
+        unsuscribe();
+      };
     }
   }, [user]);
 
+  const renderLoader = () => (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        padding: "20px",
+        alignItems: "center",
+      }}
+    >
+      <CircularProgress color="success" />
+      <span
+        style={{
+          marginLeft: "10px",
+          letterSpacing: "3px",
+        }}
+      >
+        Loading...
+      </span>
+    </div>
+  );
   return (
     <div className="app">
-      <Routes>
-        <Route
-          path="/orders"
-          element={
-            <>
-              <Header />
-              <Orders orders={orders} />
-              <Footer />
-            </>
-          }
-        />
-        <Route
-          path="/order-details/:orderId"
-          element={
-            <>
-              <Header />
-              <OrderDetails orders={orders} />
-              <Footer />
-            </>
-          }
-        />
+      <Suspense fallback={renderLoader()}>
+        <Routes>
+          <Route
+            path="/orders"
+            element={
+              <>
+                <Header />
+                <Orders orders={orders} />
+                <Footer />
+              </>
+            }
+          />
+          <Route
+            path="/order-details/:orderId"
+            element={
+              <>
+                <Header />
+                <OrderDetails orders={orders} />
+                <Footer />
+              </>
+            }
+          />
 
-        <Route
-          path="/payment"
-          element={
-            <>
-              <Header />
-              <Paymant />
-              <Footer />
-            </>
-          }
-        />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route
-          path="/checkout"
-          element={
-            <>
-              <Header />
-              <Checkout />
-              <Footer />
-            </>
-          }
-        />
-        <Route
-          path="/"
-          element={
-            <>
-              <Header />
-              <Feed />
-              <Footer />
-            </>
-          }
-        />
-        <Route
-          path="*"
-          element={
-            <>
-              <Header />
-              <NoMatch />
-              <Footer />
-            </>
-          }
-        />
-      </Routes>
+          <Route
+            path="/payment"
+            element={
+              <>
+                <Header />
+                <Paymant />
+                <Footer />
+              </>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route
+            path="/checkout"
+            element={
+              <>
+                <Header />
+                <Checkout />
+                <Footer />
+              </>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Feed />
+                <Footer />
+              </>
+            }
+          />
+          <Route
+            path="*"
+            element={
+              <>
+                <Header />
+                <NoMatch />
+                <Footer />
+              </>
+            }
+          />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
